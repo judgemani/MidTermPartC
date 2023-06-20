2c960b4e61df096dda698739be18cf2330657b51
judgemani <133786577+judgemani@users.noreply.github.com>
2023-06-20 10:33:37 -0400

    Update Mankirat_Singh_judge.java for fetch and merge

--- src/midtermexam_versiona_extensioncode/Mankirat_Singh_judge.java
+++ src/midtermexam_versiona_extensioncode/Mankirat_Singh_judge.java
@@ -7,3 +7,4 @@ public class Mankirat_Singh_judge {
 }
 //My name is Mankirat Singh Judge and I am from India. I am studying in Sheridan College.
 //adding changes on github for the mid term.
+//for fetch and merge
